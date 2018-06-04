39395eaebd54a1e87f2067fcb8d25a4d4cb584e4
Sin <sin@sin.com>
2018-06-04 23:38:28 +0200

    Fix unit test

--- src/pages/home/components/header/header-spec.js
+++ src/pages/home/components/header/header-spec.js
@@ -2,11 +2,19 @@ import React from 'react';
 import {
   mount
 } from 'enzyme';
+import {
+  MemoryRouter
+} from 'react-router-dom'
 import Header from './header';
+
 
 describe('Header', () => {
   it('should render the header component', () => {
-    const wrapper = mount(<Header language="de" t={t => t}/>);
+    const wrapper = mount(
+      <MemoryRouter>
+        <Header language="de" t={t => t}/>
+      </MemoryRouter>
+    );
     expect(wrapper.html()).to.contain('header');
   })
 });
